db5a873246ac786a989f1dfd3558ececc6402bfe
lopkors6970 <73071028+lopkors6970@users.noreply.github.com>
2020-12-13 16:55:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+website = "http://www.sadikturan.com"
+course = "Python Kursu: Baştan Sona Python Programlama Rehberiniz (40 saat)"
+
+# 1- 'course' karakter dizisinde kaç karakter bulumaktadır ?
+#result = len(course)
+#print(result)
+
+# 2- 'website' içinden www karakterlerini alma.
+#result = website[7:10] ------ burada gelir 7. indexten başlar ve 9. indexi basıp sona erer.
+
+# 3- 'website' içinden com karakterlerini alma.
+#result = website[length-3:length]
+
+
+
+
+
+
